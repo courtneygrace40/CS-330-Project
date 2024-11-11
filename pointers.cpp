c8fa136999158a54632866fb82fbf3493caa5f5b
courtneygrace40 <courtneygrace40@gmail.com>
2024-11-10 22:16:30 -0500

    Update pointers.cpp

--- pointers.cpp
+++ pointers.cpp
@@ -5,8 +5,10 @@ using namespace std;
 //This file is an example of how pointers work in C++. 
 int main(){
     int x = 136;
+    //pointer to address of x
     int *xp = &x;
     int y = 118;
+    //changes the value at the address of x
     (*xp) = 10;
     cout << x;
     xp = &y;
